a816db7f5a8fba7c4f8bd0863fe8d957ad9a698d
ibumarskov <ibumarskov@mirantis.com>
2019-09-03 16:24:59 +0400

    Combine upload and anylize actions in one script
    Additionally:
    - Add Config class to store QAR configuration
    - Add settings.py
    - Move deprecated scripts to dedicated folder

---
+++ lib/config.py
@@ -0,0 +1,9 @@
+import os
+import logging
+
+
+class Config(object):
+    def __init__(self):
+        self.url = os.environ.get('TESTRAIL_URL')
+        self.user = os.environ.get('TESTRAIL_USER')
+        self.password = os.environ.get('TESTRAIL_PASSWORD')
--- lib/reportparser.py
+++ lib/reportparser.py
@@ -5,15 +5,15 @@ class ReportParser(object):
     def __init__(self, tr_case_attrs='etc/tr_case_attrs.yaml',
                  tr_result_attrs='etc/tr_result_attrs.yaml'):
         with open(tr_case_attrs, 'r') as stream:
-            self.tr_case_attrs = yaml.load(stream)
+            self.tr_case_attrs = yaml.safe_load(stream)
         with open(tr_result_attrs, 'r') as stream:
-            self.tr_result_attrs = yaml.load(stream)
+            self.tr_result_attrs = yaml.safe_load(stream)
 
 
 class CheckListParser(object):
     def __init__(self, check_list_attrs='etc/check_list_example.yaml'):
         with open(check_list_attrs, 'r') as stream:
-            self.attrs = yaml.load(stream)
+            self.attrs = yaml.safe_load(stream)
         self._check_structure()
 
     def _check_structure(self):
