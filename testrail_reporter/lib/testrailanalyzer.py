f8053e94731298959d2163c2d628dcb1a9432d2e
Hanna Arhipova <harhipova@mirantis.com>
2021-04-14 13:32:41 +0300

    Allow defining 'title' with regex pattern
    User can use regex pattern in the 'title' field in yaml file
    (which is defined in the CLI using check_list_path parameter)
    while analizing test results
    
    It requires existing
       title_type: regexp        in the checklist.yaml
    
    Example:
      - title: deployment_*
        title_type: regexp
        errors:
          - "Some error happened"
        status: ProdFailed
        defects: PROD-0
    
    Change-Id: I88e5083d839b702a38a2ce1e5997baa755449f47

--- testrail_reporter/lib/testrailanalyzer.py
+++ testrail_reporter/lib/testrailanalyzer.py
@@ -2,6 +2,7 @@ import logging
 import sys
 
 import yaml
+import re
 
 from testrail_reporter.lib.exceptions import NotFound
 from testrail_reporter.lib.testrailproject import TestRailProject
@@ -66,7 +67,7 @@ class TestRailAnalyzer:
         if check_obj['errors']:
             for err in check_obj['errors']:
                 if not current_res['comment']:
-                    LOG.warn("Test result for {} doesn't contain any log."
+                    LOG.warning("Test result for {} doesn't contain any log."
                              "".format(test["title"]))
                     return False
                 if err in current_res['comment']:
@@ -84,9 +85,49 @@ class TestRailAnalyzer:
         LOG.info("Test '{}' set to {}".format(test["title"],
                                               check_obj['status']))
 
+    @staticmethod
+    def _match_found(pattern, text):
+        try:
+            return re.search(pattern, text)
+        except re.error as e:
+            return pattern == text
+
     def analyze_results(self, check_list_obj):
         isinstance(check_list_obj, CheckListParser)
-        for check_obj in check_list_obj.attrs['tests']:
+        check_objs_by_titles = {
+            check_obj['title']: check_obj
+            for check_obj in check_list_obj.attrs['tests']
+            if not check_obj.get('title_type', '') == 'regexp'
+        }
+
+        tests_by_titles = {
+            test['title']: test
+            for test in self.tests
+        }
+
+        titles_of_common_tests = set(tests_by_titles.keys()).\
+            intersection(check_objs_by_titles.keys())
+
+        if titles_of_common_tests.__len__():
+            LOG.debug("Next test results will be changed: {tests}".format(
+                            tests="\n".join(titles_of_common_tests)))
+
+        for title in titles_of_common_tests:
+            self._check_errors(check_objs_by_titles[title],
+                               tests_by_titles[title])
+
+        # # Check regexp-style titles
+
+        regexp_obj = [
+            check_obj
+            for check_obj in check_list_obj.attrs['tests']
+            if check_obj.get('title_type', '') == 'regexp'
+        ]
+        for check_obj in regexp_obj:
             for test in self.tests:
-                if test['title'] == check_obj['title']:
+                if test['title'] in titles_of_common_tests:
+                    continue
+                if self._match_found(check_obj['title'],
+                                     test['title']):
+                    LOG.debug(f"Found next test by regexp {test['title']}")
                     self._check_errors(check_obj, test)
