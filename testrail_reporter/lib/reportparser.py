2d1dcbdc1c0e2f407dd9903ffa403c0373f74daf
ibumarskov <ibumarskov@mirantis.com>
2022-06-14 16:57:16 +0400

    Refactor result/case maps
    Fix issue with processing results with several test suites
    
    Change-Id: If4c38369554933667799ac07b055a75445c71315

--- testrail_reporter/lib/reportparser.py
+++ testrail_reporter/lib/reportparser.py
@@ -1,82 +1,81 @@
-import copy
+import logging
 import re
+import sys
 import xml.etree.ElementTree as ET
 import yaml
 
 from testrail_reporter.lib.actions import perform_actions
-from testrail_reporter.lib.exceptions import UnknownAction
+from testrail_reporter.lib.exceptions import UnknownAction, ActionIsMissed, \
+    FieldIsMissed
+
+
+LOG = logging.getLogger(__name__)
+LOG.addHandler(logging.StreamHandler(sys.stdout))
 
 
 class ReportParser(object):
-    def __init__(self, tr_result_attrs, tr_result_map):
+    def __init__(self, tr_result_attrs):
         with open(tr_result_attrs, 'r') as stream:
             self.tr_result_attrs = yaml.safe_load(stream)
-        with open(tr_result_map, 'r') as stream:
-            self.tr_result_map = yaml.safe_load(stream)
         self.raw_results = []
 
-    def _get_result_testsuite(self, testsuite):
-        for child in testsuite:
-            if child.tag != self.tr_result_map['tc_tag']:
+    def process_testsuite(self, root):
+        assert root.tag == 'testsuite', \
+            f"Expected <testsuite> but received <{root.tag}>"
+        for child in root:
+            if child.tag != 'testcase':
+                LOG.warning(f"Expected <testcase> but received <{child.tag}>")
                 continue
-            tc_res = copy.copy(self.tr_result_attrs)
-
-            # Get test name:
-            tc_res['test_id'] = self.perform_xml_actions(
-                child, self.tr_result_map['test_id']['xml_actions']
-            )
-            if not tc_res['test_id']:
-                Exception("Test_id (title) can'be empty")
-
-            # Get test status:
-            tc_res['status_id'] = self.perform_xml_actions(
-                child, self.tr_result_map['status_id']['xml_actions']
-            )
-            if not tc_res['status_id']:
-                tc_res['status_id'] = \
-                    self.tr_result_map['status_id']['default']
-
-            # Get comments (logs):
-            tc_res['comment'] = self.perform_xml_actions(
-                child, self.tr_result_map['comment']['xml_actions']
-            )
+            tc_res = self.get_result_fields(child)
+            for field in ['test_id', 'status_id']:
+                if not tc_res[field]:
+                    FieldIsMissed(field=field)
             self.raw_results.append(tc_res)
 
     def get_result_list(self, xml_file):
         tree = ET.parse(xml_file)
         root = tree.getroot()
 
-        if root.tag == "testsuites":
-            for testsuite in root:
-                self._get_result_testsuite(testsuite)
+        if root.tag == 'testsuites':
+            for ts in root:
+                self.process_testsuite(ts)
         else:
-            self._get_result_testsuite(root)
+            self.process_testsuite(root)
 
         results = {'results': [],
                    'results_setup': [],
                    'results_teardown': []}
+        filter_res = {'filter_setup': 'results_setup',
+                      'filter_teardown': 'results_teardown'}
         for res in self.raw_results:
-            if 'filter_setup' in self.tr_result_map:
-                pattern = self.tr_result_map['filter_setup']['match']
-                actions = self.tr_result_map['filter_setup']['actions']
-                if re.match(pattern, res['test_id']):
-                    out = perform_actions(res['test_id'],
-                                          actions)
-                    res['test_id'] = out
-                    results['results_setup'].append(res)
-                    continue
-
-            if 'filter_teardown' in self.tr_result_map:
-                pattern = self.tr_result_map['filter_teardown']['match']
-                actions = self.tr_result_map['filter_teardown']['actions']
-                if re.match(pattern, res['test_id']):
-                    out = perform_actions(res['test_id'],
-                                          actions)
-                    res['test_id'] = out
-                    results['results_teardown'].append(res)
-                    continue
+            filtered = False
+            for fltr, r in filter_res.items():
+                if fltr in self.tr_result_attrs:
+                    fltr_attrs = self.tr_result_attrs[fltr]
+                    pattern = fltr_attrs['match']
+                    actions = fltr_attrs['actions']
+                    if re.match(pattern, res['test_id']):
+                        out = perform_actions(res['test_id'], actions)
+                        res['test_id'] = out
+                        replace_status = fltr_attrs.get('status')
+                        if replace_status:
+                            res['status_id'] = replace_status
+                        results[r].append(res)
+                        filtered = True
+            if filtered:
+                continue
             results['results'].append(res)
         return results
+
+    def get_result_fields(self, xml_tc):
+        tc_res = {}
+        for key, val in self.tr_result_attrs.items():
+            if key in ['filter_setup', 'filter_teardown']:
+                continue
+            tc_res[key] = self.perform_xml_actions(xml_tc, val['xml_actions'])
+            if not tc_res[key]:
+                tc_res[key] = val.get('default')
+        return tc_res
 
     @staticmethod
     def action_get_attribute(child, attr_name):
@@ -88,40 +87,6 @@ class ReportParser(object):
     def action_get_element_text(child):
         return child.text
 
-    @staticmethod
-    def action_check_child(child, attr_name):
-        for key, value in child.attrib.items():
-            if key == attr_name:
-                return value
-
-    @staticmethod
-    def check_attribute(child, attr_name):
-        if attr_name in child.attrib.keys():
-            return True
-        else:
-            return False
-
-    @staticmethod
-    def return_subchild(child, properties):
-        for subchild in child:
-            tag = False
-            attr = False
-            if 'tag' in properties:
-                if subchild.tag == properties['tag']:
-                    tag = True
-            else:
-                tag = True
-            if 'attribute' in properties:
-                for attr in subchild.attrib.keys():
-                    if attr == properties['attribute']:
-                        attr = True
-            else:
-                attr = True
-
-            if tag and attr:
-                return subchild
-        return None
-
     def perform_xml_actions(self, child, actions, res=''):
         for action in actions:
             if 'add_string' in action:
@@ -131,21 +96,13 @@ class ReportParser(object):
                 res += self.action_get_attribute(child, attr_name)
             elif 'get_element_text' in action:
                 res += self.action_get_element_text(child)
-            elif 'check' in action:
-                if 'parent' in action['check']:
-                    nested = action['check']['parent']
-                    attr_name = nested['attribute']
-                    if self.check_attribute(child, attr_name):
-                        res += self.perform_xml_actions(child,
-                                                        nested['xml_actions'],
-                                                        res=res)
-                if 'child' in action['check']:
-                    nested = action['check']['child']
-                    subchild = self.return_subchild(child, nested)
-                    if subchild is not None:
-                        res += self.perform_xml_actions(subchild,
-                                                        nested['xml_actions'],
-                                                        res=res)
+            elif 'has_child_tag' in action:
+                for subchild in child:
+                    if subchild.tag == action['has_child_tag']:
+                        if 'xml_actions' not in action:
+                            raise ActionIsMissed(action='xml_actions')
+                        res += self.perform_xml_actions(
+                            subchild, action['xml_actions'], res=res)
             else:
                 raise UnknownAction(action=action)
         return res
