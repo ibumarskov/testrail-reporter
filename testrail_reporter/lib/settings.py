8ea9acae0e8614dee2b2bed0aa8a2dc5f9578e69
Ilya Bumarskov <ibumarskov@mirantis.com>
2025-08-11 15:00:21 +0400

    Add TRR_TITLE_MAX_LENGTH
    Reduce title length according with TestRail limitations.
    
    Change-Id: I46efe914a71870b244953d4f7a70c04de69c7388

--- testrail_reporter/lib/settings.py
+++ testrail_reporter/lib/settings.py
@@ -5,3 +5,4 @@ TRR_LOG_FILE = os.environ.get(
     "TRR_LOG_FILE", os.path.join(os.getcwd(), 'testrail-reporter.log')
 )
 TRR_LOG_LEVEL = os.environ.get("TRR_LOG_LEVEL", "DEBUG")
+TRR_TITLE_MAX_LENGTH = int(os.environ.get("TRR_TITLE_MAX_LENGTH", 250))
